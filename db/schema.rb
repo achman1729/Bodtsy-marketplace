835c2d9f8eadd23686844efe10d1686eed8cc8d9
achman1729 <achin.madot@gmail.com>
2020-05-12 09:09:06 +1000

    migrated workout model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_225710) do
+ActiveRecord::Schema.define(version: 2020_05_11_230818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,4 +39,19 @@ ActiveRecord::Schema.define(version: 2020_05_11_225710) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "workouts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "price"
+    t.time "duration"
+    t.bigint "user_id", null: false
+    t.bigint "trainer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trainer_id"], name: "index_workouts_on_trainer_id"
+    t.index ["user_id"], name: "index_workouts_on_user_id"
+  end
+
+  add_foreign_key "workouts", "trainers"
+  add_foreign_key "workouts", "users"
 end
