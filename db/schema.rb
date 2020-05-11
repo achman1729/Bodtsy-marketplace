482ca9571d214e686b4e19d27c9a33753e3279ab
achman1729 <achin.madot@gmail.com>
2020-05-12 09:13:12 +1000

    migrated join table for workout and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_230936) do
+ActiveRecord::Schema.define(version: 2020_05_11_231220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2020_05_11_230936) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "workout_categories", force: :cascade do |t|
+    t.bigint "workout_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_workout_categories_on_category_id"
+    t.index ["workout_id"], name: "index_workout_categories_on_workout_id"
+  end
+
   create_table "workouts", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -58,6 +67,8 @@ ActiveRecord::Schema.define(version: 2020_05_11_230936) do
     t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
+  add_foreign_key "workout_categories", "categories"
+  add_foreign_key "workout_categories", "workouts"
   add_foreign_key "workouts", "trainers"
   add_foreign_key "workouts", "users"
 end
