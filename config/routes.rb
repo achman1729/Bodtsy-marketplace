f5926f35d20c873489ffa72a3316ab9a1d008b0d
achman1729 <achin.madot@gmail.com>
2020-05-12 09:47:48 +1000

    generated cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'workouts/index'
-  get 'workouts/new'
-  get 'workouts/create'
-  get 'workouts/show'
-  get 'workouts/edit'
-  get 'workouts/update'
-  get 'workouts/destroy'
+
   devise_for :trainers
   devise_for :users
-  root 'home#index'
+  root 'workouts#index'
+  resources :workouts
+  resources :carts, only: [:show, :edit, :update, :destroy]
 end
