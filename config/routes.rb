3691af49f025c0b5342230d5f8a420bb1c1fc456
achman1729 <achin.madot@gmail.com>
2020-05-12 09:38:51 +1000

    added workouts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'workouts/index'
+  get 'workouts/new'
+  get 'workouts/create'
+  get 'workouts/show'
+  get 'workouts/edit'
+  get 'workouts/update'
+  get 'workouts/destroy'
   devise_for :trainers
   devise_for :users
   root 'home#index'
