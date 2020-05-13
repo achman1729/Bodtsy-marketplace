8782afcf9dd0ebd76e9ed08f7bc387afe47c54bf
achman1729 <achin.madot@gmail.com>
2020-05-13 18:13:53 +1000

    added logic to application controller for redirecting after signinig in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-
+    #   for redirecting user after signing in
+    def after_sign_in_path_for(resource)
+        if current_user.account_type == "trainer"
+            trainer_path
+        else
+            workouts_path
+        end
+      end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,4 +1,13 @@
 class TrainersController < ApplicationController
+  # before_action :authenticate_trainer
   def index
+    @workout = Workout.all
+  end
+
+  private
+
+  def authenticate_trainer
+    if current_user.account_type == "trainer"
+    end
   end
 end
