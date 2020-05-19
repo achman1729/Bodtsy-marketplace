dd65feda7455fb17f8f69a466b3bbbb6238ac998
achman1729 <achin.madot@gmail.com>
2020-05-19 21:49:12 +1000

    resolves conflict in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-  has_one :cart, dependent: :destroy
-=======
   has_many :carts, dependent: :destroy # a cart is a cart item
->>>>>>> 608f87b
   has_many :user_workouts
   has_many :workouts, through: :user_workouts
 end
