b139feae4c79b68ea9680d0f4932fda55b513429
achman1729 <achin.madot@gmail.com>
2020-05-19 21:56:32 +1000

    resolved conflict in environment.js file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,15 +2,6 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-<<<<<<< HEAD
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
-=======
 const { environment } = require('@rails/webpacker')
 
 const { environment } = require('@rails/webpacker')
@@ -28,4 +19,3 @@ Popper: ['popper.js', 'default']
 }))
 
 module.exports = environment
->>>>>>> 608f87b
